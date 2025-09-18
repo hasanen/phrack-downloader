32d53077d8be53fe3f86aee33274ac1b3237ab7f
Joni Hasanen <joni@pieceofcode.com>
2025-09-18 16:01:32 +0300

    Add some options that might be useful

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -1,6 +1,19 @@
+use std::path::PathBuf;
+
 // SPDX-License-Identifier: MIT
+use crate::models::issue::Issue;
 use crate::phrack_issue_manager_error::PhrackIssueManagerError;
 
+pub enum ExportFormat {
+    TXT,
+}
+pub struct ExportOptions {
+    pub format: ExportFormat,
+    // Output folder for the exported file
+    pub output_folder: PathBuf,
+    pub issue: Issue,
+}
+
 pub trait Exporter {
-    fn export(&self) -> Result<(), PhrackIssueManagerError>;
+    fn export(&self, options: &ExportOptions) -> Result<(), PhrackIssueManagerError>;
 }
